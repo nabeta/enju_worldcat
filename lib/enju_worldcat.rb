e19e47386f2954f507d7b0078c2c9d4a682f7172
Kosuke Tanabe <nabeta@x-il.jp>
2009-06-29 03:48:37 +0900

    use oclc_number by default

--- lib/enju_worldcat.rb
+++ lib/enju_worldcat.rb
@@ -1,3 +1,4 @@
+require 'will_paginate'
 module EnjuWorldcat
   def self.included(base)
     base.extend ClassMethods
@@ -21,15 +22,19 @@ module EnjuWorldcat
       nil
     end
 
-    def search_worldcat_by_isbn(isbn)
-      @@client.GetRecord(:type => 'isbn', :id => isbn).record
+    def search_worldcat_by_id(id, options = {:type => 'isbn'})
+      @@client.GetRecord(:type => options[:type], :id => id).record
     end
   end
   
   module InstanceMethods
     @@client = WCAPI::Client.new(:wskey => WORLDCAT_API_KEY)
     def worldcat_record
-      @@client.GetRecord(:type => 'isbn', :id => self.isbn).record
+      if self.oclc_number.present?
+        @@client.GetRecord(:id => self.oclc_number).record
+      else
+        @@client.GetRecord(:type => 'isbn', :id => self.isbn).record
+      end
     rescue
       nil
     end
@@ -41,7 +46,11 @@ module EnjuWorldcat
       @@client.GetCitation(:type => 'isbn', :id => self.isbn)
     end
 
-    def xisbn_manifestations
+    def xisbn_manifestations(options = {})
+      page = options[:page] || 1
+      per_page = options[:per_page] || 10
+      total_entries = options[:total_entries]
+
       doc = REXML::Document.new open("http://xisbn.worldcat.org/webservices/xid/isbn/#{self.isbn}?method=getEditions&format=xml&fl=*")
       isbn_array = REXML::XPath.match(doc, '/rsp/isbn/')
       manifestations = []
@@ -55,7 +64,12 @@ module EnjuWorldcat
         manifestation[:lccn] = isbn.attribute('lccn').to_s
         manifestations << manifestation if manifestation[:isbn] != self.isbn
       end
-      return manifestations
+      WillPaginate::Collection.create(page, per_page, total_entries) do |pager|
+        pager.total_entries = manifestations.size
+        isbns = manifestations[(page - 1) * per_page, per_page]
+        pager.replace isbns
+      end
     end
+
   end
 end
